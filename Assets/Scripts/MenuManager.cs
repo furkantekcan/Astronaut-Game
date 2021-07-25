63d4bde4d4eb6ab663e9ebb37f06f39adae84256
furkantekcan <l6A14006@std.yildiz.edu.tr>
2021-07-25 19:41:35 +0300

    pause menu added

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject gameScreen, pauseScreen;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +21,31 @@ public class MenuManager : MonoBehaviour
     public void PlayButton()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void PauseButton()
+    {
+        Time.timeScale = 0f;
+        gameScreen.SetActive(false);
+        pauseScreen.SetActive(true);
+    }
+
+    public void ResumeButton()
+    {
+        Time.timeScale = 1f;
+        gameScreen.SetActive(true);
+        pauseScreen.SetActive(false);
+    }
+
+    public void ReplayButton()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void HomeButton()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
     }
 }
